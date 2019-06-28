7d8a0041c8938eb6b6db731d58a8d5d1681631ed
Daniel Bonniot <dbonniot@chemaxon.com>
2019-06-28 12:56:38 +0200

    Add spinner

--- ui.js
+++ ui.js
@@ -19,6 +19,28 @@ const tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
+const opts = {
+  lines: 12, // The number of lines to draw
+  length: 0, // The length of each line
+  width: 18, // The line thickness
+  radius: 43, // The radius of the inner circle
+  scale: 1.0, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  color: '#000000', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  speed: 0.5, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-fade-default', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  className: 'spinner', // The CSS class to assign to the spinner
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  position: 'absolute' // Element positioning
+};
+const spinner = new Spinner(opts).spin(d3.select("#graph")[0][0]);
+
 let savedRanking = null;
 let xType = "rank";
 
@@ -29,6 +51,7 @@ function xAxisChange(radio) {
 
 function updateRanking(ranking) {
 	savedRanking = ranking;
+	spinner.stop();
 	displayRanking();
 }
 
